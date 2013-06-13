c10f9c671bf38de93b27e673ddeca009d1c6044b
David Fetter <david@fetter.org>
2013-06-12 20:15:56 -0700

    Actually fixed cherry-pick conflicts.  :P

--- src/backend/utils/adt/inversedistribution.c
+++ src/backend/utils/adt/inversedistribution.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*-------------------------------------------------------------------------
  *
  * inversedistribution.c
@@ -13,13 +12,10 @@
  *
  *-------------------------------------------------------------------------
  */
-=======
->>>>>>> 2f3bbf6... First cut at patch.
 #include "postgres.h"
 #include "fmgr.h"
 #include <string.h>
 
-<<<<<<< HEAD
 /*
  * percentile_disc(float8)  - discrete (nearest) percentile
  */
@@ -32,14 +28,6 @@ percentile_disc(PG_FUNCTION_ARGS)
 	elog(ERROR, "not implemented yet");
 }
 
-=======
-#ifdef PG_MODULE_MAGIC
-PG_MODULE_MAGIC;
-#endif
-
-Datum percentile_disc(PG_FUNCTION_ARGS);
-
->>>>>>> 2f3bbf6... First cut at patch.
 PG_FUNCTION_INFO_V1(percentile_disc);
 
 Datum
@@ -51,11 +39,12 @@ percentile_disc_final(PG_FUNCTION_ARGS)
 	test_text = PG_GETARG_TEXT_P(0);
 	PG_RETURN_TEXT_P(test_text);
 }
-<<<<<<< HEAD
 
 /*
  * percentile_cont(float8)  - continuous (nearest) percentile
  */
+Datum percentile_cont(PG_FUNCTION_ARGS);
+
 Datum
 percentile_cont(PG_FUNCTION_ARGS)
 {
@@ -73,5 +62,3 @@ percentile_cont_final(PG_FUNCTION_ARGS)
 	test_text = PG_GETARG_TEXT_P(0);
 	PG_RETURN_TEXT_P(test_text);
 }
-=======
->>>>>>> 2f3bbf6... First cut at patch.
