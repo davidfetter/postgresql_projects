7b6358c2a86621a7b59c19f20c3597c96dad027a
Karol Trzcionka <karlik@fedoraproject.org>
2013-07-19 22:56:00 +0200

    Minor cleanup

--- src/backend/optimizer/plan/setrefs.c
+++ src/backend/optimizer/plan/setrefs.c
@@ -1697,25 +1697,25 @@ fix_join_expr_mutator(Node *node, fix_join_expr_context *context)
 			context->root->parse->commandType == CMD_UPDATE)
 		{
 			RangeTblEntry *rte_a, *rte_r;
-			ListCell *rte_r_cell;
 			rte_a = (RangeTblEntry *)list_nth(context->root->parse->rtable,var->varno-1);
 			if (rte_a->rtekind == RTE_BEFORE && strcmp(rte_a->eref->aliasname,"after") == 0)
 			{
 				if (var->varno > 2)
 				{
-					rte_r_cell = list_nth_cell(context->root->parse->rtable,var->varno-3);
-					rte_r = (RangeTblEntry *)lfirst(rte_r_cell);
+					rte_r = (RangeTblEntry *)list_nth(context->root->parse->rtable,var->varno-3);
 					if(rte_r->rtekind == RTE_RELATION && rte_r->relid == rte_a->relid)
 						var->varno -= 2;
 
-					rte_r = (RangeTblEntry *)lnext(rte_r_cell);
-					if(rte_r->rtekind == RTE_RELATION && rte_r->relid == rte_a->relid)
-						var->varno -= 1;
+					else
+					{
+						rte_r = (RangeTblEntry *)list_nth(context->root->parse->rtable,var->varno-2);
+						if(rte_r->rtekind == RTE_RELATION && rte_r->relid == rte_a->relid)
+							var->varno -= 1;
+					}
 				}
 				else
 				{
-					rte_r_cell = list_nth_cell(context->root->parse->rtable,var->varno-2);
-					rte_r = (RangeTblEntry *)lfirst(rte_r_cell);
+					rte_r = (RangeTblEntry *)list_nth(context->root->parse->rtable,var->varno-2);
 					if(rte_r->rtekind == RTE_RELATION && rte_r->relid == rte_a->relid)
 						var->varno -= 1;
 				}
