4a84d571eaacd72e996bcdf138f47f108e315349
Atri <atri.jiit@gmail.com>
2014-08-10 21:41:43 +0530

    Added more regression tests

--- src/test/regress/sql/groupingsets.sql
+++ src/test/regress/sql/groupingsets.sql
@@ -28,4 +28,10 @@ select a, b from (values (1,2),(2,3)) v(a,b) group by grouping sets((a,b),());
 
 select a, b from (values (1,2),(2,3)) v(a,b) group by rollup((a,b));
 
+select a, b, sum(c) from (values (1,1,10,5),(1,1,11,5),(1,2,12,5),(1,2,13,5),(1,3,14,5),(2,3,15,5),(3,3,16,5),(3,4,17,5),(4,1,18,5),(4,1,19,5)) v(a,b,c,d) group by rollup ((a,b));
+
+create temp view tv2(a,b,c,d,e,f,g) as select a[1], a[2], a[3], a[4], a[5], a[6], generate_series(1,3) from (select (array[1,1,1,1,1,1])[1:6-i] || (array[2,2,2,2,2,2])[7-i:6] as a from generate_series(0,6) i) s;
+
+select a,b, sum(g) from tv2 group by grouping sets ((a,b,c),(a,b));
+
 
