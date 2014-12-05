240e702f28a814a872def2840cb39c46dc61fa3f
Atri <atri.jiit@gmail.com>
2014-12-05 11:54:36 +0530

    Added more regression tests

--- src/test/regress/sql/preorder_check.sql
+++ src/test/regress/sql/preorder_check.sql
@@ -7,6 +7,10 @@ INSERT INTO preorder_test2 VALUES(100,105);
 INSERT INTO preorder_test2 VALUES(95,110);
 INSERT INTO preorder_test2 VALUES(57,112);
 INSERT INTO preorder_test2 VALUES(95,110);
+INSERT INTO preorder_test2 VALUES(57,NULL);
+INSERT INTO preorder_test2 VALUES(27,NULL);
+INSERT INTO preorder_test2 VALUES(NULL,107);
+INSERT INTO preorder_test2 VALUES(NULL,NULL);
 
 
 INSERT INTO preorder_test VALUES(5,6);
@@ -18,7 +22,8 @@ CREATE OR REPLACE FUNCTION correct_order_singlecol(OUT e int, OUT f int) returns
 CREATE OR REPLACE FUNCTION incorrect_order_singlecol(OUT e int, OUT f int) returns setof record as ' SELECT a,b FROM preorder_test; ' language 'sql' ORDER BY e;
 CREATE OR REPLACE FUNCTION correct_order_multicol(OUT e int, OUT f int) returns setof record as ' SELECT a,b FROM preorder_test ORDER BY a,b; ' language 'sql' ORDER BY e,f;
 CREATE OR REPLACE FUNCTION multiple_tables_correct(OUT e int, OUT f int, OUT g int, OUT h int) returns setof record as ' SELECT * FROM preorder_test,preorder_test2 ORDER BY a,b,i; ' language 'sql' ORDER BY e,f,g;
-
+CREATE OR REPLACE FUNCTION correct_order_nulls(OUT e int, OUT f int) returns setof record as ' SELECT i,u FROM preorder_test2 ORDER BY i,u NULLS FIRST; ' language 'sql' ORDER BY e,f NULLS FIRST;
+CREATE OR REPLACE FUNCTION incorrect_order_nulls(OUT e int, OUT f int) returns setof record as ' SELECT i,u FROM preorder_test2 ORDER BY i,u NULLS LAST; ' language 'sql' ORDER BY e,f NULLS FIRST;
 
 SELECT * FROM correct_order_singlecol();
 
@@ -33,6 +38,10 @@ SELECT * FROM incorrect_order_singlecol() ORDER BY e,f;
 SELECT * FROM correct_order_multicol() ORDER BY e,f;
 SELECT * FROM correct_order_multicol() ORDER BY e;
 SELECT * FROM correct_order_multicol() ORDER BY f;
+SELECT * FROM correct_order_nulls() ORDER BY e NULLS FIRST;
+SELECT * FROM correct_order_nulls() ORDER BY e NULLS LAST;
+SELECT * FROM incorrect_order_nulls() ORDER BY e NULLS FIRST;
+SELECT * FROM incorrect_order_nulls() ORDER BY e NULLS LAST;
 
 EXPLAIN (COSTS OFF)SELECT * FROM incorrect_order_singlecol();
 EXPLAIN (COSTS OFF) SELECT * FROM correct_order_singlecol() ORDER BY e;
@@ -49,6 +58,10 @@ EXPLAIN (COSTS OFF) SELECT * FROM correct_order_singlecol() WHERE e=17 ORDER BY 
 EXPLAIN (COSTS OFF) SELECT * FROM multiple_tables_correct() ORDER BY e;
 EXPLAIN (COSTS OFF) SELECT * FROM multiple_tables_correct() ORDER BY e,f;
 EXPLAIN (COSTS OFF) SELECT * FROM correct_order_singlecol() WHERE f=110 ORDER BY e;
+EXPLAIN (COSTS OFF) SELECT * FROM correct_order_nulls() ORDER BY e,f NULLS FIRST;
+EXPLAIN (COSTS OFF) SELECT * FROM correct_order_nulls() ORDER BY e,f NULLS LAST;
+EXPLAIN (COSTS OFF) SELECT * FROM incorrect_order_nulls() ORDER BY e NULLS FIRST;
+EXPLAIN (COSTS OFF) SELECT * FROM incorrect_order_nulls() ORDER BY e NULLS LAST;
 
 DROP FUNCTION correct_order_singlecol();
 DROP FUNCTION incorrect_order_singlecol();
