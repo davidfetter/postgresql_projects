459322e908821b5230c738fa3a56c2758edee28b
Atri <atri.jiit@gmail.com>
2015-07-20 20:01:09 +0530

    More fixes

--- src/backend/executor/nodeAgg.c
+++ src/backend/executor/nodeAgg.c
@@ -1882,6 +1882,7 @@ agg_fill_hash_table(AggState *aggstate)
 	TupleTableSlot *outerslot;
 	int maxsets = aggstate->maxsets;
 	int i = 0;
+	bool isNull = false;
 
 	/*
 	 * get state info from node
@@ -1894,13 +1895,16 @@ agg_fill_hash_table(AggState *aggstate)
 	 * Process each outer-plan tuple, and then fetch the next one, until we
 	 * exhaust the outer plan.
 	 */
-	for (i = 0;i < maxsets;i++)
+	for (;;)
 	{
-		for (;;)
+		for (i = 0;i < maxsets;i++)
 		{
 			outerslot = fetch_input_tuple(aggstate);
 			if (TupIsNull(outerslot))
+			{
+				isNull = true;
 				break;
+			}
 			/* set up for advance_aggregates call */
 			tmpcontext->ecxt_outertuple = outerslot;
 
@@ -1913,6 +1917,9 @@ agg_fill_hash_table(AggState *aggstate)
 			/* Reset per-input-tuple context after each tuple */
 			ResetExprContext(tmpcontext);
 		}
+
+		if (isNull)
+			break;
 	}
 
 	aggstate->table_filled = true;
